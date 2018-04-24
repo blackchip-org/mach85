5619bbf42adf5945f709eeb0afb000b535e09041
Mike McGann <mike.mcgann@blackchip.org>
2018-04-24 18:36:38 -0400

    a greeting is nice

--- cmd/mach85/main.go
+++ cmd/mach85/main.go
@@ -8,10 +8,14 @@ import (
 
 func main() {
 	log.SetFlags(0)
+
+	log.Printf("\nWelcome to the Mach-85!\n\n")
+
 	mach := mach85.New()
 	if err := mach.LoadROM(); err != nil {
 		log.Fatal(err)
 	}
+	log.Println()
 	mon := mach85.NewMonitor(mach)
 	mon.Run()
 }
